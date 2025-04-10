a3988c394377b9cf5906d94997e1d330926c3672
sparrow-js <sparrowwht7@gmail.com>
2025-04-10 22:04:09 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL('https://images.unsplash.com/**')],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
